a40fb74d1d67fb93683816b8c0a285c2370bee64
MichaelHerrera-Ing <maicolherrera675@gmail.com>
2024-08-03 21:32:15 -0500

    Initial commit with portfolio files

---
+++ my-portfolio/src/app/components/contact/contact.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent {
+  name: string = '';
+  email: string = '';
+  message: string = '';
+
+  onSubmit() {
+    alert(`Nombre: ${this.name}\nEmail: ${this.email}\nMensaje: ${this.message}`);
+  }
+}
